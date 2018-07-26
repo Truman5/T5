004be34e8435d1ace795a5ca28ebd72134df2274
Truman5 <39314512+Truman5@users.noreply.github.com>
2018-07-26 15:24:16 -0400

    Add files via upload

---
+++ gamescript.js
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+<SCRIPT LANGUAGE= "javascript">
+setTimeout("document.bgColor = 'white'", 1000)
+setTimeout("document.bgColor = 'lightpink'", 1500)
+setTimeout("document.bgColor = 'pink'", 2000)
+setTimeout("document.bgColor = 'darksalmon'", 2500)
+setTimeout("document.bgColor = 'plum'", 3000)
+setTimeout("document.bgColor = 'tomato'", 3500)
+setTimeout("document.bgColor = 'peru'", 4000)
+setTimeout("document.bgColor = 'cornsilk'", 4500)
+setTimeout("document.bgColor = 'yellow'", 5000)
+setTimeout("document.bgColor = 'rosybrown'", 5500)
+setTimeout("document.bgColor = 'sandybrown'", 6000)
+</SCRIPT>
+</body>
+</html>
