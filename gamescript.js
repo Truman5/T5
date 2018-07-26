617aec38ca61e90a4346325a79e427844679e858
Truman5 <39314512+Truman5@users.noreply.github.com>
2018-07-26 16:25:16 -0400

    Update gamescript.js

--- gamescript.js
+++ gamescript.js
@@ -1,4 +1,3 @@
-
 setTimeout("document.bgColor = 'white'", 1000)
 setTimeout("document.bgColor = 'lightpink'", 1500)
 setTimeout("document.bgColor = 'pink'", 2000)
@@ -10,4 +9,3 @@ setTimeout("document.bgColor = 'cornsilk'", 4500)
 setTimeout("document.bgColor = 'yellow'", 5000)
 setTimeout("document.bgColor = 'rosybrown'", 5500)
 setTimeout("document.bgColor = 'sandybrown'", 6000)
-
